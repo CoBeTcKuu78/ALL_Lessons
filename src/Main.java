a81fa4b97c8b6172a6fc0be8eb360ddbbc81ab4d
CoBeTcKuu <9437855@gmail.com>
2022-08-27 14:45:06 +0300

    коммит 1 урок - ПЕРЕМЕННЫЕ

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,57 @@
 public class Main {
     public static void main(String[] args) {
-        var box = 5;
-        System.out.println(box);
+        // УРОК № 1
+        /* var liftingCapacity = 50;
+        var stuffWeight = 20;
+        var capasityLeft = liftingCapacity - stuffWeight;
+        System.out.println("Еще можно нагрузить " + capasityLeft + " кг");
+
+        var applesWeight = 2;
+        var orangesWeights = 3;
+        var fruitsWeights = applesWeight + orangesWeights;
+        System.out.println("Общий вес фруктов " + fruitsWeights);
+
+        var meatWeight = 4;
+        var waterWeight = 5;
+        var tomatoesWeight = 2;
+        var cucumberWeight = 2;
+        var pepperWeights = 2;
+        var zucchiniWeight = 2;
+        var vegetablesWeight = tomatoesWeight + cucumberWeight + pepperWeights + zucchiniWeight;
+        var productsWeight = fruitsWeights + waterWeight + meatWeight + vegetablesWeight;
+        System.out.println("Общий вес продуктов " + productsWeight + " кг!");
+
+        var leftWeight = liftingCapacity - stuffWeight - productsWeight;
+        System.out.println("Места осталось " + leftWeight + " кг!");
+
+        productsWeight = productsWeight * 2;
+        System.out.println("Теперь вес всех продуктов " + productsWeight + " кг!");
+
+        leftWeight = liftingCapacity - stuffWeight - productsWeight;
+        System.out.println("Теперь места осталось " + leftWeight + " кг!");
+
+        var owerload = (stuffWeight + productsWeight) % liftingCapacity;
+        System.out.println("Перегруз на " + owerload + " кг!");
+
+        var productsInOneCar = productsWeight / 2;
+        System.out.println("Вещей в одной машине " + productsInOneCar + " кг!");
+
+        short bananas = 200;
+        System.out.println("Бананов " + bananas + " кг!");
+
+        double sugar = 3;
+        double onePortion = sugar / 4;
+        System.out.println("Вес одной порции сахара " + onePortion + " кг!");
+
+        byte a = 1;
+        short b = 1;
+        int c = 1;
+        int d = a + b + c;
+        System.out.println(d);
+
+        float g = a + 1f;
+        System.out.println(g); */
+        // УРОК № 2
+
     }
 }
